dad2e2f686c7e99d5250c583fa760682f33169f5
Amisevic <amisevic@etfos.hr>
2025-04-25 10:04:45 +0200

    dodani fileovi

---
+++ my-project/public/script.js
@@ -0,0 +1,28 @@
+fetch('filmtv_movies.csv')
+    .then(res => res.text())
+    .then( csv => {
+        const rez = Papa.parse(csv, {
+            header: true,
+            skipEmptyLines: true
+        });
+        const filmovi = rez.data.map(film => ({
+            title: film.title,
+            year: Number(film.year),
+            genre: film.genre,
+            duration: Number(film.duration),
+            country: film.country?.split(',').map(c => c.trim()) || [],
+            total_votes: Number(film.total_votes)
+            }));
+        const prvih20 = filmovi.slice(0, 150);
+        prikaziTablicu(prvih20);
+    });
+
+function prikaziTablicu(filmovi) {
+    const tbody = document.querySelector('#filmovi-tablica tbody');
+    tbody.innerHTML = ''; // ocisti ako postoji
+    for (const film of filmovi) {
+        const row = document.createElement('tr');
+        row.innerHTML = '<td>${film.title}</td><td>${film.year}</td><td>${film.genre}</td> <td>${film.duration}</td> <td>${film.country.join(', ')}</td> <td>${film.total_votes}</td>';
+        tbody.appendChild(row);
+        }
+    }
---
+++ my-project/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// Poslužuj statičke datoteke (HTML, CSS, JS, CSV)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, () => {
+console.log(`Server je pokrenut na http://localhost:${port}`);
+});
